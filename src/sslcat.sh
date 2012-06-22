e48e461a94f096bc315a10f5351a70f95d799805
William Erik Baxter <web@superscript.com>
2012-06-22 06:03:38 -0400

    Port base tests to new build.

--- src/sslcat.sh
+++ src/sslcat.sh
@@ -1,6 +1,6 @@
 host=${1-0}
 shift
-port=${2-443}
+port=${1-443}
 shift
 exec #HOME#/command/sslclient -RHl0 ${1+"$@"} -- "$host" "$port" sh -c 'exec cat <&6'
 
--- src/sslconnect.sh
+++ src/sslconnect.sh
@@ -1,5 +1,5 @@
 host=${1-0}
 shift
-port=${2-443}
+port=${1-443}
 shift
 exec #HOME#/command/sslclient -RHl0 ${1+"$@"} -- $host $port #TCPBIN#/mconnect-io
